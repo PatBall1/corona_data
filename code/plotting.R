7431b945d82c35151f1f73c49b5d15b4c789e9dd
PatBall1 <ball.jgc@gmail.com>
2020-03-30 10:57:31 +0100

    cleared clutter

--- code/plotting.R
+++ code/plotting.R
@@ -6,20 +6,21 @@ library(tidyr)
 library(ggplot2)
 
 data("coronavirus")
-coronavirus <- as_tibble(coronavirus)
+corona.data <- as_tibble(coronavirus)
 corona.sp <- coronavirus_spatial()
 corona.sp$iso_n3 <- as.integer(corona.sp$iso_n3)
 corona.sp <- as_tibble(corona.sp) %>%
-  select(Country.Region, iso_n3, -geometry)
-corona <- full_join(corona.sp, coronavirus, by="Country.Region")
+  select(iso_n3)
+corona.data <- bind_cols(corona.sp, corona.data)
 
 data(pop)
 pop <- as_tibble(pop) %>%
   select(country_code, "2020") %>%
-  rename(iso_n3=country_code, population.2020="2020")
+  rename(iso_n3=country_code, population.2020="2020") %>%
+  mutate(population.2020=population.2020*1000)
 
 
-corona <- left_join(corona, pop, by="iso_n3")
+corona.data <- left_join(corona.data, pop, by="iso_n3")
 
 
 #setdiff(pop$name, coronavirus$Country.Region)
@@ -30,26 +31,30 @@ corona <- left_join(corona, pop, by="iso_n3")
 
 paste("Lastest date", max(coronavirus$date))
 
-#corona <- as_tibble(coronavirus) %>%
+#corona <- corona.data %>%
 #  filter(Province.State=="") %>%
-#  select(-3, -4) %>%
+#  select(-4, -5) %>%
 #  mutate(cum.cases=ave(cases, Country.Region, type, FUN=cumsum))
 
-corona <- as_tibble(corona) %>%
+corona <- as_tibble(corona.data) %>%
   filter(Province.State=="") %>%
   select(-4, -5) %>%
   group_by(Country.Region, type) %>%
-  mutate(cum.cases=cumsum(cases))
+  mutate(cum.cases=cumsum(cases)) %>%
+  mutate(cases.rel=cases/population.2020) %>%
+  mutate(cum.cases.rel=cum.cases/population.2020)
 
+#countries <- sample(unique(corona$Country.Region), 15)
+countries <- c("United Kingdom", "Korea, South", "Spain", "France", "Italy", "Germany","US", "Sweden")
+#countries <- "Italy"
 
-countries <- sample(unique(corona$Country.Region), 15)
-countries <- c("United Kingdom", "Korea, South", "Spain", "France", "Italy", "Germany")
+case.type <- "death"
 
-case.type <- "confirmed"
+##### PLOTS #####
 
 corona %>%
   filter(type==case.type, Country.Region %in% countries, date>"2020-02-20") %>%
-  ggplot(aes(x=date, y=cum.cases/population.2020)) +
+  ggplot(aes(x=date, y=log(cum.cases.rel))) +
   ggtitle(case.type) +
   geom_smooth(aes(col=Country.Region, linetype=Country.Region), size=1) 
 
@@ -65,30 +70,11 @@ corona %>%
   ggtitle(case.type) +
   geom_smooth(aes(col=Country.Region), size=1)
 
+##### SUMMARY #####
+
 summary_df <- coronavirus %>%
   filter(Province.State=="") %>%
   group_by(Country.Region, type) %>%
   summarise(total_cases = sum(cases)) %>%
   arrange(-total_cases)
 
-populations <- pop %>%
-  select(name, "2020") %>%
-  rename(Country.Region=name)
-
-left_join(summary_df, populations)
-
-UK_cases <- as_tibble(coronavirus) %>%
-  filter(Country.Region=="United Kingdom", Province.State=="", type=="confirmed") %>%
-  mutate(cum.cases=cumsum(cases))
-
-Korea <- coronavirus %>%
-  filter(Country.Region=="Korea, South")
-
-cases <- 
-  filter(Korea_cases, type=="confirmed")
-deaths <- filter(Korea_cases, type=="death")
-
-
-
-plot(cases$date, cumsum(cases$cases), type='l')
-lm(cases ~ date , cases)
